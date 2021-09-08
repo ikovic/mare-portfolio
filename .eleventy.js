d61d41f00d6737619f03571f57315dc3292a2d37
ikovic <kovic.ivan@gmail.com>
2021-09-08 16:06:07 +0200

    Refactor for readability

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,48 @@ const { minify } = require('terser');
 const path = require('path');
 
 /**
+ * Generates a readable image file name.
+ * @param {*} id
+ * @param {*} src
+ * @param {*} width
+ * @param {*} format
+ * @param {*} options
+ * @returns
+ */
+function getImageFilename(id, src, width, format, options) {
+  const extension = path.extname(src);
+  const name = path.basename(src, extension);
+
+  // id is hash based, this gives us readable file names
+  return `${name}-${width}w.${format}`;
+}
+
+function createResponsiveImageGenerator(widths) {
+  return async function getResponsiveImage(src, alt, sizes = '100vw', htmlClass = '') {
+    let metadata = await Image(src, {
+      widths,
+      formats: ['avif', 'webp', 'jpeg'],
+      outputDir: './_site/images/optimized/',
+      urlPath: '/images/optimized/',
+      filenameFormat: getImageFilename,
+    });
+
+    let imageAttributes = {
+      alt,
+      sizes,
+      loading: 'lazy',
+      decoding: 'async',
+      class: htmlClass,
+    };
+
+    // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+    return Image.generateHTML(metadata, imageAttributes, {
+      whitespaceMode: 'inline',
+    });
+  };
+}
+
+/**
  * Returns a link styled as a button.
  * @param {*} text
  * @param {*} htmlClass
@@ -14,8 +56,10 @@ function buttonShortcode(text, href, htmlClass = '') {
   return `<a class="button ${htmlClass}" href="${href}">${text}</a>`;
 }
 
+const thumbnailShortcode = createResponsiveImageGenerator([760]);
+
 /**
- * WIP Generates markup required for lightGallery to use picture elements with responsive images.
+ * Generates markup required for lightGallery to use picture elements with responsive images.
  * @param {*} mainSrc
  * @param {*} alt
  * @returns
@@ -27,13 +71,7 @@ async function galleryItemShortcode(mainSrc, alt) {
     formats: ['webp', 'jpeg'],
     outputDir: './_site/images/optimized/',
     urlPath: '/images/optimized/',
-    filenameFormat: function (id, src, width, format, options) {
-      const extension = path.extname(src);
-      const name = path.basename(src, extension);
-
-      // id is hash based, this gives us readable file names
-      return `${name}-${width}w.${format}`;
-    },
+    filenameFormat: getImageFilename,
   });
 
   const srcSetWebp = stats.webp.map(({ srcset }) => srcset).join(', ');
@@ -47,110 +85,30 @@ async function galleryItemShortcode(mainSrc, alt) {
   return `<a data-src="${stats.jpeg[0].url}" data-sources='${srcSet}'>${thumbnail}</a>`;
 }
 
-/**
- * Generates an optimized image for the given src.
- * @param {*} src
- * @param {*} alt
- * @param {*} sizes
- * @returns a shortcode to be used in templates (eg: {% image "images/pencil.jpeg", "photo of my pencil", "(min-width: 30em) 50vw, 100vw" %})
- */
-async function thumbnailShortcode(src, alt, sizes = '100vw', htmlClass = '') {
-  let metadata = await Image(src, {
-    widths: [760],
-    formats: ['avif', 'webp', 'jpeg'],
-    outputDir: './_site/images/optimized/',
-    urlPath: '/images/optimized/',
-    filenameFormat: function (id, src, width, format, options) {
-      const extension = path.extname(src);
-      const name = path.basename(src, extension);
-
-      // id is hash based, this gives us readable file names
-      return `thumb-${name}-${width}w.${format}`;
-    },
-  });
-
-  let imageAttributes = {
-    alt,
-    sizes,
-    loading: 'lazy',
-    decoding: 'async',
-    class: htmlClass,
-  };
-
-  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
-  return Image.generateHTML(metadata, imageAttributes, {
-    whitespaceMode: 'inline',
-  });
-}
-
-/**
- * Generates an optimized image for the given src.
- * @param {*} src
- * @param {*} alt
- * @param {*} sizes
- * @returns a shortcode to be used in templates (eg: {% image "images/pencil.jpeg", "photo of my pencil", "(min-width: 30em) 50vw, 100vw" %})
- */
-async function imageShortcode(src, alt, sizes = '100vw', htmlClass = '') {
-  let metadata = await Image(src, {
-    widths: [320, 760, 1120],
-    formats: ['avif', 'webp', 'jpeg'],
-    outputDir: './_site/images/optimized/',
-    urlPath: '/images/optimized/',
-    filenameFormat: function (id, src, width, format, options) {
-      const extension = path.extname(src);
-      const name = path.basename(src, extension);
-
-      // id is hash based, this gives us readable file names
-      return `${name}-${width}w.${format}`;
-    },
-  });
-
-  let imageAttributes = {
-    alt,
-    sizes,
-    loading: 'lazy',
-    decoding: 'async',
-    class: htmlClass,
-  };
-
-  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
-  return Image.generateHTML(metadata, imageAttributes, {
-    whitespaceMode: 'inline',
-  });
-}
-
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('js');
   eleventyConfig.addPassthroughCopy('images');
 
   /* gallery - lightbox */
   eleventyConfig.addPassthroughCopy({
     './node_modules/lightgallery/css/lightgallery-bundle.min.css': 'lib/lightgallery.css',
-  });
-  eleventyConfig.addPassthroughCopy({
     './node_modules/lightgallery/lightgallery.min.js': 'lib/lightgallery.js',
-  });
-  eleventyConfig.addPassthroughCopy({
     './node_modules/lightgallery/images/loading.gif': 'images/loading.gif',
-  });
-  eleventyConfig.addPassthroughCopy({
     './node_modules/lightgallery/fonts/lg.ttf': 'fonts/lg.ttf',
-  });
-  eleventyConfig.addPassthroughCopy({
     './node_modules/lightgallery/fonts/lg.woff': 'fonts/lg.woff',
   });
 
   /* gallery - masonry */
   eleventyConfig.addPassthroughCopy({
     './node_modules/justifiedGallery/dist/css/justifiedGallery.min.css': 'lib/justifiedGallery.css',
-  });
-  eleventyConfig.addPassthroughCopy({
     './node_modules/justifiedGallery/dist/js/jquery.justifiedGallery.min.js':
       'lib/justifiedGallery.js',
   });
 
   // add a template shortcode to replace image references with optimized ones
-  eleventyConfig.addNunjucksAsyncShortcode('image', imageShortcode);
+  eleventyConfig.addNunjucksAsyncShortcode(
+    'image',
+    createResponsiveImageGenerator([320, 760, 1120])
+  );
   eleventyConfig.addNunjucksAsyncShortcode('thumb', thumbnailShortcode);
   eleventyConfig.addNunjucksAsyncShortcode('gallery', galleryItemShortcode);
 
